dd6e4b25275c238d8cc0125ebde4f477c16ad271
Azabu Mai <mgwk1120@outlook.com>
2026-06-03 23:36:04 +0800

    lfu

---
+++ include/LFUcache.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "cache_base.h"
+namespace CacheSpace
+{
+    template <typename Key, typename Value>
+    class LFUcache : public CacheBase<Key, Value>
+    {
+    protected:
+    public:
+    };
+}
